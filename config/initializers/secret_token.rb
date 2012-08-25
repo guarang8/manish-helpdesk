e7b8ba95ff15d4983188209195d3a0e358172aa6
manishmsn <manish.gauranga@gmail.com>
2012-08-25 23:03:08 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManishHelpdesk::Application.config.secret_token = '5e25015b96fc643f17751523a1e5cd1b0dbde57e0c647db91b73b006608eefd33446cb2884ce4c7357f31072996dc49f8e031b1abbd320dda851078293052348'
